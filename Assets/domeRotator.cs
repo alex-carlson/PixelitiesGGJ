fe5394c3472f24a9cb222b1cc08c1489f0c40d57
Joseph Jolton <j@jolton.com>
2016-01-30 10:44:00 -0600

    Cloud domes

---
+++ Assets/domeRotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class domeRotator : MonoBehaviour
+{
+	public int speed = 10;
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Rotate (Vector3.forward * Time.deltaTime * speed);
+
+	}
+}
