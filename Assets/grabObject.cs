71bb294e62e78ec60d864daead039e8b851fdeef
Alex Carlson <alex@acwd.me>
2016-01-29 22:16:58 -0600

    added basic raycast script

---
+++ Assets/grabObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class grabObject : MonoBehaviour {
+
+	void Start() {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+		Debug.DrawLine (transform.position, fwd, Color.red);
+
+		RaycastHit hit;
+
+		if (Physics.Raycast(transform.position, transform.forward, out hit)) {
+			if (hit.transform.tag == "Pickup") {
+				Debug.Log (hit.transform.gameObject);
+			}
+		}
+	}
+}
